47fd73dbe395249d6fbe31c647174ae3693a82fe
Vinicius Carvalho <viniciusccarvalho@gmail.com>
2016-04-27 04:14:26 -0400

    Quotes boot

---
+++ quotes-service/src/main/java/io/pivotal/boot/samples/QuotesServiceApplication.java
@@ -0,0 +1,19 @@
+package io.pivotal.boot.samples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class QuotesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotesServiceApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+}
